9917cdd2d6695e493c9d0aacb0679a2e863a31ee
HuanSK <huan_wang@westpac.co.nz>
2017-06-06 15:41:28 +1200

    save from f b about new idea

--- notebooks/Shared/IFRS/testOneGit.scala
+++ notebooks/Shared/IFRS/testOneGit.scala
@@ -3,8 +3,16 @@
 
 // COMMAND ----------
 
-//feature one is on its way. A conert in the desert. Saved locally. 
+//feature one is on its way. A concert in the desert. Saved locally. 
 
 // COMMAND ----------
 
-//save feature one to git
+//save fe
+
+// COMMAND ----------
+
+//from master: that sounds horrible. do a concert in city
+
+// COMMAND ----------
+
+//from feature: new venue needed. also, so many typos
